eef1241c3bab63ced0ca84be23284859560c442e
IpvLeo <leoanarcopunk77@gmail.com>
2024-03-20 17:35:10 -0300

    fazendo component e melhorando antigos já feitos

--- suinocultura/src/app/app-routing.module.ts
+++ suinocultura/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AutenticacaoComponent } from './autenticacao/autenticacao.component';
 import { CadastroSuinoComponent } from './cadastro-suino/cadastro-suino.component';
 import { ControlePesoComponent } from './controle-peso/controle-peso.component';
 import { ListagemSuinosComponent } from './listagem-suinos/listagem-suinos.component';
+import { ManejoSanitarioComponent } from './manejo-sanitario/manejo-sanitario.component';
 
 
 
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'listagem-suinos', component: ListagemSuinosComponent },
   { path: 'cadastro-suino', component: CadastroSuinoComponent },
   { path: 'controle-peso', component: ControlePesoComponent },
+  { path: 'manejo-sanitario', component: ManejoSanitarioComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
